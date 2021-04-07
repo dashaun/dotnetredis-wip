f375f995ca5bc0f926bd8d57774bea999270e73e
DaShaun <dashaun@redislabs.com>
2021-04-07 10:57:04 -0500

    merged from other repo

--- Program.cs
+++ Program.cs
@@ -17,35 +17,29 @@ namespace dotnetredis
     {
         private const string SecretName = "CacheConnection";
 
-        private static Lazy<ConnectionMultiplexer> lazyConnection = CreateConnection();
-        private static long lastReconnectTicks = DateTimeOffset.MinValue.UtcTicks;
-        private static DateTimeOffset firstErrorTime = DateTimeOffset.MinValue;
-        private static DateTimeOffset previousErrorTime = DateTimeOffset.MinValue;
+        private static Lazy<ConnectionMultiplexer> _lazyConnection = CreateConnection();
+        private static long _lastReconnectTicks = DateTimeOffset.MinValue.UtcTicks;
+        private static DateTimeOffset _firstErrorTime = DateTimeOffset.MinValue;
+        private static DateTimeOffset _previousErrorTime = DateTimeOffset.MinValue;
 
-        private static readonly object reconnectLock = new();
+        private static readonly object ReconnectLock = new();
 
         private static IConfigurationRoot Configuration { get; set; }
 
-        public static ConnectionMultiplexer Connection => lazyConnection.Value;
+        private static ConnectionMultiplexer Connection => _lazyConnection.Value;
 
-        // In general, let StackExchange.Redis handle most reconnects,
-// so limit the frequency of how often ForceReconnect() will
-// actually reconnect.
-        public static TimeSpan ReconnectMinFrequency => TimeSpan.FromSeconds(60);
+        private static TimeSpan ReconnectMinFrequency => TimeSpan.FromSeconds(60);
 
-// If errors continue for longer than the below threshold, then the
-// multiplexer seems to not be reconnecting, so ForceReconnect() will
-// re-create the multiplexer.
-        public static TimeSpan ReconnectErrorThreshold => TimeSpan.FromSeconds(30);
+        private static TimeSpan ReconnectErrorThreshold => TimeSpan.FromSeconds(30);
 
-        public static int RetryMaxAttempts => 5;
+        private static int RetryMaxAttempts => 5;
 
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
+        private static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
@@ -79,75 +73,60 @@ namespace dotnetredis
             }
             catch (Exception)
             {
-                // Example error condition: if accessing oldConnection.Value causes a connection attempt and that fails.
+                // Error
             }
         }
 
-        /// <summary>
-        ///     Force a new ConnectionMultiplexer to be created.
-        ///     NOTES:
-        ///     1. Users of the ConnectionMultiplexer MUST handle ObjectDisposedExceptions, which can now happen as a result of
-        ///     calling ForceReconnect().
-        ///     2. Don't call ForceReconnect for Timeouts, just for RedisConnectionExceptions or SocketExceptions.
-        ///     3. Call this method every time you see a connection exception. The code will:
-        ///     a. wait to reconnect for at least the "ReconnectErrorThreshold" time of repeated errors before actually
-        ///     reconnecting
-        ///     b. not reconnect more frequently than configured in "ReconnectMinFrequency"
-        /// </summary>
-        public static void ForceReconnect()
+        private static void ForceReconnect()
         {
             var utcNow = DateTimeOffset.UtcNow;
-            var previousTicks = Interlocked.Read(ref lastReconnectTicks);
+            var previousTicks = Interlocked.Read(ref _lastReconnectTicks);
             var previousReconnectTime = new DateTimeOffset(previousTicks, TimeSpan.Zero);
             var elapsedSinceLastReconnect = utcNow - previousReconnectTime;
 
-            // If multiple threads call ForceReconnect at the same time, we only want to honor one of them.
             if (elapsedSinceLastReconnect < ReconnectMinFrequency)
                 return;
 
-            lock (reconnectLock)
+            lock (ReconnectLock)
             {
                 utcNow = DateTimeOffset.UtcNow;
                 elapsedSinceLastReconnect = utcNow - previousReconnectTime;
 
-                if (firstErrorTime == DateTimeOffset.MinValue)
+                if (_firstErrorTime == DateTimeOffset.MinValue)
                 {
-                    // We haven't seen an error since last reconnect, so set initial values.
-                    firstErrorTime = utcNow;
-                    previousErrorTime = utcNow;
+                    _firstErrorTime = utcNow;
+                    _previousErrorTime = utcNow;
                     return;
                 }
 
-                if (elapsedSinceLastReconnect < ReconnectMinFrequency)
-                    return; // Some other thread made it through the check and the lock, so nothing to do.
+                if (elapsedSinceLastReconnect < ReconnectMinFrequency){
+                    return;
+                }
 
-                var elapsedSinceFirstError = utcNow - firstErrorTime;
-                var elapsedSinceMostRecentError = utcNow - previousErrorTime;
+                var elapsedSinceFirstError = utcNow - _firstErrorTime;
+                var elapsedSinceMostRecentError = utcNow - _previousErrorTime;
 
                 var shouldReconnect =
                     elapsedSinceFirstError >=
-                    ReconnectErrorThreshold // Make sure we gave the multiplexer enough time to reconnect on its own if it could.
+                    ReconnectErrorThreshold
                     && elapsedSinceMostRecentError <=
-                    ReconnectErrorThreshold; // Make sure we aren't working on stale data (e.g. if there was a gap in errors, don't reconnect yet).
+                    ReconnectErrorThreshold; 
 
-                // Update the previousErrorTime timestamp to be now (e.g. this reconnect request).
-                previousErrorTime = utcNow;
+                _previousErrorTime = utcNow;
 
                 if (!shouldReconnect)
                     return;
 
-                firstErrorTime = DateTimeOffset.MinValue;
-                previousErrorTime = DateTimeOffset.MinValue;
+                _firstErrorTime = DateTimeOffset.MinValue;
+                _previousErrorTime = DateTimeOffset.MinValue;
 
-                var oldConnection = lazyConnection;
+                var oldConnection = _lazyConnection;
                 CloseConnection(oldConnection);
-                lazyConnection = CreateConnection();
-                Interlocked.Exchange(ref lastReconnectTicks, utcNow.UtcTicks);
+                _lazyConnection = CreateConnection();
+                Interlocked.Exchange(ref _lastReconnectTicks, utcNow.UtcTicks);
             }
         }
-
-// In real applications, consider using a framework such as
-// Polly to make it easier to customize the retry approach.
+        
         private static T BasicRetry<T>(Func<T> func)
         {
             var reconnectRetry = 0;
@@ -187,12 +166,12 @@ namespace dotnetredis
         {
             return BasicRetry(() => Connection.GetServer(host, port));
         }
-        
+
         public static HashEntry[] ToHashEntries(object obj)
         {
             PropertyInfo[] properties = obj.GetType().GetProperties();
             return properties
-                .Where(x => x.GetValue(obj) != null) // <-- PREVENT NullReferenceException
+                .Where(x => x.GetValue(obj) != null)
                 .Select
                 (
                     property =>
@@ -200,13 +179,8 @@ namespace dotnetredis
                         object propertyValue = property.GetValue(obj);
                         string hashValue;
 
-                        // This will detect if given property value is 
-                        // enumerable, which is a good reason to serialize it
-                        // as JSON!
                         if (propertyValue is IEnumerable<object>)
                         {
-                            // So you use JSON.NET to serialize the property
-                            // value as JSON
                             hashValue = JsonConvert.SerializeObject(propertyValue);
                         }
                         else
